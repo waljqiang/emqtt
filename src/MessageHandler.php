61d074d3a22608454d1ce8ef205acbaea92e1f15
waljqiang <zql185@163.com>
2020-01-06 16:36:44 +0800

    message handle remove mqtt

--- src/MessageHandler.php
+++ src/MessageHandler.php
@@ -2,48 +2,47 @@
 namespace Waljqiang\Mqtt;
 
 use sskaje\mqtt\MessageHandler as BaseHandler;
-use Waljqiang\Mqtt\Mqtt;
 
 class MessageHandler extends BaseHandler{
 	public $waitQueue = [];
 
-	public function onConnack(Mqtt $mqtt, $connack_object){
+	public function onConnack($mqtt, $connack_object){
 		
     }
 
-    public function onDisconnect(Mqtt $mqtt){
+    public function onDisconnect($mqtt){
     	
     }
 
-    public function onSuback(Mqtt $mqtt, $suback_object){
+    public function onSuback($mqtt, $suback_object){
     	
     }
 
-    public function onUnsuback(Mqtt $mqtt, $unsuback_object){
+    public function onUnsuback($mqtt, $unsuback_object){
     	
     }
 
-    public function onPublish(Mqtt $mqtt, $public_object){
+    public function onPublish($mqtt, $public_object){
     	
     }
 
-    public function onPuback(Mqtt $mqtt, $puback_object){
+    public function onPuback($mqtt, $puback_object){
     	
     }
 
-    public function onPubrec(Mqtt $mqtt, $pubrec_object){
+    public function onPubrec($mqtt, $pubrec_object){
     	
     }
 
-    public function onPubrel(MQTT $mqtt, $pubrel_object){
+    public function onPubrel($mqtt, $pubrel_object){
     	
     }
 
-    public function onPubcomp(Mqtt $mqtt,$pubcomp_object){
+    public function onPubcomp($mqtt,$pubcomp_object){
 
     }
 
-    public function onPingresp(Mqtt $mqtt,$pingresp_object){
+    public function onPingresp($mqtt,$pingresp_object){
 
     }
 
